677e92f5bf891cfb900f7786b29d17e211603738
Ujang Arisandi <ujangaja@gmail.com>
2017-10-09 16:58:25 +0700

    Edit Model PurchaseOrder

--- app/PurchaseOrder.php
+++ app/PurchaseOrder.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class PurchaseOrder extends Model
 {
-    //
+    public $incrementing = false;
+    protected $primaryKey = 'ref';
+    protected $keyType = 'string';
+    protected $dates = [
+    	'date',
+    	'updated_at',
+    	'created_at',
+    ];
+    protected $casts = [
+    	'received' => 'bolean',
+    ];
+    public function purchase_items(){
+    	return $this->hasMany('App\PurchaseItem');
+    }
 }
