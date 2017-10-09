b9c8e58d842eb7c75966756d4cf7efd98947860e
Ujang Arisandi <ujangaja@gmail.com>
2017-10-09 17:12:30 +0700

    Edit Model SaleItem

--- app/SaleItem.php
+++ app/SaleItem.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class SaleItem extends Model
 {
-    //
+    public function product()
+    {
+    	return $this->belongsTo('App\Product');
+    }
+
+    public function sale_order()
+    {
+    	return $this->belongsTo('App\SaleOrder');
+    }
 }
