7e3ccc9a852b3cd628748e2e36acb9dcf89eb449
Ujang Arisandi <ujangaja@gmail.com>
2017-10-09 17:07:52 +0700

    edit Model SaleOrder

--- app/SaleOrder.php
+++ app/SaleOrder.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class SaleOrder extends Model
 {
-    //
+    public $incrementing = false;
+    protected $primaryKey = 'ref';
+    protected $keyType = 'string';
+    protected $dates = [
+    	'date',
+    	'updated_at',
+    	'created_at',
+    ];
+    public function sale_items()	
+    {
+    	return $this->hasMany('App\SaleItem');
+    }
 }
