2794c4598fb19ae6115ad6aa1ffc5dafe8095329
Ujang Arisandi <ujangaja@gmail.com>
2017-10-09 17:03:17 +0700

    Edit Model PurchaseItem

--- app/PurchaseItem.php
+++ app/PurchaseItem.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class PurchaseItem extends Model
 {
-    //
+    public function product(){
+    	return $this->belongsTo('App\Product');
+    }
+
+    public function purchase_order(){
+    	return $this->belongsTo('App\PurchaseOrder');
+    }
 }
